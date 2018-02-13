04c47a4566850dfcd95687d9c2aea6bb5a300176
Kit Gardner <gamesdevkit@gmail.com>
2018-02-12 19:14:39 -0700

    Added UI elements and sounds. Tweak some level interaction.

---
+++ Assets/Script/TreeHitScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeHitScript : MonoBehaviour
+{
+    
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.name.Contains("Tree"))
+        {
+            print("I hit a tree!");
+            AudioManager.audInstance.PlayTreeHitSound();
+        }
+    }
+}
